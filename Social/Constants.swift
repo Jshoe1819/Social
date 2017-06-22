543d02348123d2568b2ff6c6100d585c3cfba5d9
Jacob Shoemaker <Jshoe1819@gmail.com>
2017-06-22 19:26:21 -0400

    Sign in ui custom classes complete

---
+++ Social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Social
+//
+//  Created by Jacob Shoemaker on 6/22/17.
+//  Copyright © 2017 Jacob Shoemaker. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
