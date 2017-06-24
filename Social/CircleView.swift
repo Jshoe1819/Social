1c982067f26409ecaf0bb6cbd5763657eede74eb
Jacob Shoemaker <Jshoe1819@gmail.com>
2017-06-23 21:20:07 -0400

    Built field UI

--- Social/CircleView.swift
+++ Social/CircleView.swift
@@ -1,26 +1,29 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  Social
 //
-//  Created by Jacob Shoemaker on 6/22/17.
+//  Created by Jacob Shoemaker on 6/23/17.
 //  Copyright © 2017 Jacob Shoemaker. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-    
-    
-    
+class CircleView: UIImageView {
+
     override func awakeFromNib() {
+        
         super.awakeFromNib()
         
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
         
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
